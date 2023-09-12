c58a7323f02bab2d8ec249067aa4e1206a91a5c0
Quin <quin.marilyn05@gmail.com>
2023-09-12 14:26:57 -0600

    Update the build script to process all files and also gave each file ocntent.

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
 @echo off
-python genify.py -i index.html -t templates/footer.html -t templates/header.html
+setlocal enabledelayedexpansion
+
+for %%f in (*.html) do (
+	echo Processing file: %%f
+	python genify.py -i %%f -t templates/footer.html -t templates/header.html
+)
+
+endlocal
 pause

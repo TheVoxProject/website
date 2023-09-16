55698b36c95634a54ac2cdc32a249af8cbd03074
Quin <quin.marilyn05@gmail.com>
2023-09-15 20:53:36 -0600

    Add timeline rendering to the build script.

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ for %%f in (*.html) do (
 	echo Processing file: %%f
 	python genify.py -i %%f -t templates/footer.html -t templates/header.html
 )
+python sheetable.py https://docs.google.com/spreadsheets/d/e/2PACX-1vTtn0gDzr-kM-NwW9G60kFBv-EP_k8IxHIrYs6bVIyvNNCxdxHqaZi6rnmAX4cIdyIH9lPEOFU6FA3k/pub?output=csv -o templates/timeline.html
 
 endlocal
 pause

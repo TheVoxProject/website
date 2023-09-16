a808f3a162513fb8fc6f1277ee1e21cb4d517cfa
Quin <quin.marilyn05@gmail.com>
2023-09-15 20:55:20 -0600

    Actually render the timeline on the page. I don't even want to imagine how visually awful this must look.

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 for %%f in (*.html) do (
 	echo Processing file: %%f
-	python genify.py -i %%f -t templates/footer.html -t templates/header.html
+	python genify.py -i %%f -t templates/footer.html -t templates/header.html -t templates/timeline.html
 )
 python sheetable.py https://docs.google.com/spreadsheets/d/e/2PACX-1vTtn0gDzr-kM-NwW9G60kFBv-EP_k8IxHIrYs6bVIyvNNCxdxHqaZi6rnmAX4cIdyIH9lPEOFU6FA3k/pub?output=csv -o templates/timeline.html
 

b38f5a6b85324bf67d92e11a865f9621e51f1b9e
Quin <quin.marilyn05@gmail.com>
2023-09-19 20:42:08 -0600

    move the sheetable call before the HTML rendering, because duh! WTF, self.

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
+python sheetable.py https://docs.google.com/spreadsheets/d/e/2PACX-1vTtn0gDzr-kM-NwW9G60kFBv-EP_k8IxHIrYs6bVIyvNNCxdxHqaZi6rnmAX4cIdyIH9lPEOFU6FA3k/pub?output=csv -o templates/timeline.html
 for %%f in (*.html) do (
 	echo Processing file: %%f
 	python genify.py -i %%f -t templates/footer.html -t templates/header.html -t templates/timeline.html
 )
-python sheetable.py https://docs.google.com/spreadsheets/d/e/2PACX-1vTtn0gDzr-kM-NwW9G60kFBv-EP_k8IxHIrYs6bVIyvNNCxdxHqaZi6rnmAX4cIdyIH9lPEOFU6FA3k/pub?output=csv -o templates/timeline.html
 
 endlocal
 pause
